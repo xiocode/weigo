f3d8d9421e73da7a7c3cf85682f275026af75242
Tony.Shao <xiocode@gmail.com>
2013-02-27 11:48:54 +0800

    init

---
+++ weibo/weibo.go
@@ -0,0 +1,55 @@
+/**
+ * Author: xio
+ * Date: 13-2-27
+ * Version: 0.01
+ */
+package weibo
+
+import (
+	// "json"
+	"fmt"
+	"log"
+)
+
+const (
+	HTTP_GET    int = 0
+	HTTP_POST   int = 1
+	HTTP_UPLOAD int = 2
+)
+
+//METHOD_MAP = { 'GET': _HTTP_GET, 'POST': _HTTP_POST, 'UPLOAD': _HTTP_UPLOAD }
+
+func httpCall(url string, method int, authorization string, kws ...interface{}) {
+	log.Logger.Println(url)
+	log.Logger.Println(int)
+	log.Logger.Println(authorization)
+	log.Logger.Println(kws)
+}
+
+func HttpGet(url string, authorization string, kws ...interface{}) {
+	return httpCall(url, HTTP_GET, authorization, kws)
+}
+
+func HttpPost(url string, authorization string, kws ...interface{}) {
+	return httpCall(url, HTTP_POST, authorization, kws)
+}
+
+func HttpUpload(url string, authorization string, kws ...interface{}) {
+	return httpCall(url, HTTP_UPLOAD, authorization, kws)
+}
+
+type HttpObject struct {
+	client APIClient
+	method string
+}
+
+type APIClient struct {
+	app_key       string
+	app_secret    string
+	redirect_uri  string
+	response_type string
+	domain        string
+	version       string
+	access_token  string
+	expires       float32
+}
---
+++ weibo/weibo_test.go
@@ -0,0 +1,9 @@
+package weibo
+
+import (
+	"testing"
+)
+
+func TestHttpCall(t *testing.T) {
+	HttpUpload("111", "222", "333", "13123123123", "12312312")
+}

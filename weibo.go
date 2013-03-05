16d574fb35c3d87b1e3d595404ac33b836e48a7f
Tony.Shao <xiocode@gmail.com>
2013-03-05 14:01:10 +0800

    Update

--- weibo.go
+++ weibo.go
@@ -94,8 +94,8 @@ func httpCall(the_url string, method int, authorization string, params map[strin
 	body := read_body(response)
 	result = parse_json(body)
 
-	if error_code, ok := result["error_code"].(int); ok {
-		panic(&APIError{when: time.Now(), error_code: error_code, message: result["error"].(string)})
+	if error_code, ok := result["error_code"].(string); ok {
+		panic(&APIError{when: time.Now(), error_code: strconv.Itoa(error_code), message: result["error"].(string)})
 	}
 
 	return result

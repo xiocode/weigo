acee6aab5bde4bb8e38afdbeea27a454334ff230
Tony.Shao <xiocode@gmail.com>
2013-03-05 14:10:45 +0800

    Update

--- weibo.go
+++ weibo.go
@@ -16,7 +16,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	// "reflect"
+	"reflect"
 	"strings"
 	// "sync"
 	"log"
@@ -92,8 +92,10 @@ func httpCall(the_url string, method int, authorization string, params map[strin
 	body := read_body(response)
 	result = parse_json(body)
 
-	if error_code, ok := result["error_code"].(string); ok {
-		panic(&APIError{when: time.Now(), error_code: error_code, message: result["error"].(string)})
+	if error_code, ok := result["error_code"]; ok {
+		fmt.Println(reflect.TypeOf(error_code))
+		fmt.Println(error_code)
+		// panic(&APIError{when: time.Now(), error_code: error_code, message: result["error"].(string)})
 	}
 
 	return result

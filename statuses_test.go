bd12b41151b6f4e808f1f912a80b38e74cca40dd
Tony.Shao <xiocode@gmail.com>
2013-06-24 16:16:15 +0800

    pass check

--- statuses_test.go
+++ statuses_test.go
@@ -14,16 +14,16 @@ type Times struct {
 func init() {
 	if api == nil {
 		api = NewAPIClient("3231340587", "702b4bcc6d56961f569943ecee1a76f4", "http://2.xweiboproxy.sinaapp.com/callback.php", "code")
-		api.SetAccessToken("2.00VBqgvCZS4gWDb3940dd56eFfitSB", 1524137445)
+		api.SetAccessToken("2.00VBqgvCZS4gWDb3940dd56eFfitSB", 1529741330)
 	}
 }
 
 func Test_GET_statuses_user_timeline(t *testing.T) {
 	t.SkipNow()
 	kws := map[string]interface{}{
-		"mid": "yybyOssa9",
-		// "type":     1,
-		// "isBase62": 1,
+		"mid":      "yybyOssa9",
+		"type":     1,
+		"isBase62": 1,
 	}
 	var result string
 	err := api.GET_statuses_queryid(kws, &result)
@@ -31,52 +31,56 @@ func Test_GET_statuses_user_timeline(t *testing.T) {
 	debugPrintln(result)
 }
 
-// func Test_GET_statuses_home_timeline(t *testing.T) {
-// 	kws := map[string]interface{}{
-// 		"uid": "2684726573",
-// 	}
-// 	result := new(Statuses)
-// 	err := api.GET_statuses_home_timeline(kws, result)
-// 	debugCheckError(err)
-// 	debugPrintln(len(*result.Statuses))
-// }
+func Test_GET_statuses_home_timeline(t *testing.T) {
+	t.SkipNow()
+	kws := map[string]interface{}{
+		"uid": "2684726573",
+	}
+	result := new(Statuses)
+	err := api.GET_statuses_home_timeline(kws, result)
+	debugCheckError(err)
+	debugPrintln(len(*result.Statuses))
+}
 
-// func Test_GET_statuses_repost_timeline(t *testing.T) {
-// 	kws := map[string]interface{}{
-// 		"id": "3551749023600582",
-// 	}
-// 	result := new(Reposts)
-// 	err := api.GET_statuses_repost_timeline(kws, result)
-// 	debugCheckError(err)
-// 	debugPrintln(len(*result.Reposts))
-// }
+func Test_GET_statuses_repost_timeline(t *testing.T) {
+	t.SkipNow()
+	kws := map[string]interface{}{
+		"id": "3551749023600582",
+	}
+	result := new(Reposts)
+	err := api.GET_statuses_repost_timeline(kws, result)
+	debugCheckError(err)
+	debugPrintln(len(*result.Reposts))
+}
 
-// func Test_POST_statuses_repost(t *testing.T) {
-// 	kws := map[string]interface{}{
-// 		"id": "3551749023600582",
-// 	}
-// 	result := new(Status)
-// 	err := api.POST_statuses_repost(kws, result)
-// 	debugCheckError(err)
-// 	debugPrintln(*result)
-// }
+func Test_POST_statuses_repost(t *testing.T) {
+	t.SkipNow()
+	kws := map[string]interface{}{
+		"id": "3551749023600582",
+	}
+	result := new(Status)
+	err := api.POST_statuses_repost(kws, result)
+	debugCheckError(err)
+	debugPrintln(*result)
+}
 
-// func Test_POST_statuses_repost(t *testing.T) {
-// 	kws := map[string]interface{}{
-// 		"status": "Testing...Testing...",
-// 	}
-// 	result := new(Status)
-// 	err := api.POST_statuses_update(kws, result)
-// 	debugCheckError(err)
-// 	debugPrintln(*result)
-// }
+func Test_POST_statuses_update(t *testing.T) {
+	t.SkipNow()
+	kws := map[string]interface{}{
+		"status": "Testing...Testing...",
+	}
+	result := new(Status)
+	err := api.POST_statuses_update(kws, result)
+	debugCheckError(err)
+	debugPrintln(*result)
+}
 
-// func Test_POST_statuses_repost(t *testing.T) {
-// 	kws := map[string]interface{}{
-// 		"id": "3556138715301190",
-// 	}
-// 	result := new(Status)
-// 	err := api.POST_statuses_destroy(kws, result)
-// 	debugCheckError(err)
-// 	debugPrintln(*result)
-// }
+func Test_POST_statuses_destory(t *testing.T) {
+	kws := map[string]interface{}{
+		"id": "3592749137795387",
+	}
+	result := new(Status)
+	err := api.POST_statuses_destroy(kws, result)
+	debugCheckError(err)
+	debugPrintln(*result)
+}
--- weibo_test.go
+++ weibo_test.go
@@ -1,32 +1,30 @@
 package weigo
 
 import (
-	// "bytes"
 	"fmt"
-	// "os"
-	// "reflect"
+	"reflect"
 	"testing"
 )
 
-// func TestGetAuthorizeUrl(t *testing.T) {
-// 	api := NewAPIClient("3417104247", "f318153f6a80329f06c1d20842ee6e91", "http://127.0.0.1/callback", "code")
-// 	authorize_url, err := api.GetAuthorizeUrl(nil)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	fmt.Println(authorize_url)
-// }
+func TestGetAuthorizeUrl(t *testing.T) {
+	t.SkipNow()
+	authorize_url, err := api.GetAuthorizeUrl(nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(authorize_url)
+}
 
-// func TestRequestAccessToken(t *testing.T) {
-// 	api := NewAPIClient("3417104247", "f318153f6a80329f06c1d20842ee6e91", "http://127.0.0.1/callback", "code")
-// 	var result map[string]interface{}
-// 	err := api.RequestAccessToken("1fdaa295b73d2a9568e284383ced5e9e", &result)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	fmt.Println(result)
-// 	access_token := result["access_token"]
-// 	fmt.Println(reflect.TypeOf(access_token), access_token)
-// 	expires_in := result["expires_in"]
-// 	fmt.Println(reflect.TypeOf(expires_in), expires_in)
-// }
+func TestRequestAccessToken(t *testing.T) {
+	t.SkipNow()
+	var result map[string]interface{}
+	err := api.RequestAccessToken("1fdaa295b73d2a9568e284383ced5e9e", &result)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(result)
+	access_token := result["access_token"]
+	fmt.Println(reflect.TypeOf(access_token), access_token)
+	expires_in := result["expires_in"]
+	fmt.Println(reflect.TypeOf(expires_in), expires_in)
+}
